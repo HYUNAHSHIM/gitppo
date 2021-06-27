4a73adc88f3684a8d6d64dbec47f43e5a08c1a0b
sunung007@hanyang.ac.kr <KellanLukas1103!>
2021-06-27 23:33:31 +0900

    git page

---
+++ src/git/data.js
@@ -0,0 +1,36 @@
+ export const data = [
+   {
+     "title": "gippo",
+     "readme": "readme link",
+     "image": "image link",
+     "language": ["C", "Java", "JavaScript"],
+
+     "description": "",
+     "role": "",
+     "skills": "",
+     "implement": "",
+   },
+   {
+     "title": "hyu-life",
+     "readme": "readme link",
+     "image": "image link",
+     "language": ["C", "Java", "JavaScript"],
+
+     "description": "",
+     "role": "",
+     "skills": "",
+     "implement": "",
+   },
+   {
+     "title": "hacc",
+     "readme": "readme link",
+     "image": "image link",
+     "language": ["C", "Java", "JavaScript"],
+
+     "description": "",
+     "role": "",
+     "skills": "",
+     "implement": "",
+   },
+
+];
