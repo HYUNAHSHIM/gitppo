72990023bfa26bb9e5693dbbe3b4dfd03636a1fa
HYUNAHSHIM <hyunahshim@naver.com>
2021-08-13 00:49:13 +0900

    add

--- src/result/index.js
+++ src/result/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./index.css";
 import head from "./head.png";
 import { Doughnut } from 'react-chartjs-2';
-import domtoimage from "dom-to-image";
 import jsPDF from 'jspdf';
+import html2canvas from "html2canvas";
 import { useLocation } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
 
@@ -18,27 +18,21 @@ const style = {
     height: "130px"
 }
 
-const skills = [];
-const levels = [];
-
 function Skill({ skill }) {
-    const skill2 = useLocation().state.skillList;
-    skills.push(skill.skill);
-    levels.push(skill.level);
-    const skillData = {
-        labels: skills,
-        datasets: [{
-            data: levels,
-            backgroundColor: colors,
-        }],
-    };
-    if (levels.length == skill2.length) {
-        return (
-            <Doughnut data={skillData} options={options} style={style} />
-        )
+    let level = "";
+    if (skill.level === "10") {
+        level = "초급"
+    } else if (skill.level === "20") {
+        level = "중급"
     } else {
-        return ("")
+        level = "고급"
     }
+    return (
+        <div style={{display: "flex", padding: "5px 0px", justifyContent: "space-between"}}>
+            <div className="tempbtn">{skill.skill}</div>
+            <div>{level}</div>
+        </div>
+    )
 }
 
 function Language({ lang }) {
@@ -72,18 +66,28 @@ function Company({ company }) {
 }
 
 function Work({ company }) {
-    const sdate = company.start.split("-");
-    const edate = company.end.split("-");
-    const date = sdate[0] + "." + sdate[1] + "-" + edate[0] + "." + edate[1]
     return (
-        <span className="c">{date}</span>
+        <span className="c">
+            <div style={{width: "300px", paddingTop: "15px", fontSize: "0.9em"}}>
+                {company.start + "~" + company.end}
+            </div>
+            <div style={{width: "100px", fontWeight: "700"}}>
+                {company.company}
+            </div>
+        </span>
     )
 }
 
 function School({ school }) {
     const schoolName = school.schoolName;
     return (
-        <span className="c">{schoolName}</span>
+        <span className="c">
+            <div style={{width: "100px", paddingTop: "15px"}}>
+                <span style={{fontWeight: "700"}}>{schoolName}</span>
+                <br></br>
+                <span style={{fontSize: "0.9em"}}>{school.school}</span>
+            </div>
+        </span>
     )
 }
 
@@ -93,15 +97,14 @@ function Repo({ repo }) {
     for (let i = 0; i < keys.length; i++) {
         langlist.push(keys[i])
     }
-    console.log(repo)
     return (
         <div className="project flex">
             <div className="repo">
-                <h3>{repo.name}</h3>
+                <h3 className="title">{repo.name}</h3>
                 <h6>{repo.description}</h6>
-                <h6><b>역할 | </b>{repo.input.role}</h6>
-                <h6><b>사용 기술 | </b>{repo.input.skill}</h6>
-                <h6><b>구현 내용 | </b>{repo.input.implement}</h6>
+                <h6><b>역할 | </b>{repo.input.role || "없음"}</h6>
+                <h6><b>사용 기술 | </b>{repo.input.skill || "없음"}</h6>
+                <h6><b>구현 내용 | </b>{repo.input.implement || "없음"}</h6>
                 <br></br>
                 {langlist.map(language => (
                     <span className="tempbtn">{language}</span>
@@ -116,6 +119,7 @@ function Repo({ repo }) {
 
 function Preview() {
     const location = useLocation();
+    console.log(location.state);
     const birth = location.state.birth.split('-');
     const companys = location.state.companyList;
     const skills = location.state.skillList;
@@ -149,7 +153,7 @@ function Preview() {
     return (
         <div ref={ref}>
             <div id="box">
-                <div className="content">
+                <div className="content" id="capture">
                     <div className="profile">
                         <div className="img">
                             <img src={head} id="head"></img>
@@ -161,30 +165,37 @@ function Preview() {
                                 <h1>{location.state.name}</h1>
                             </div>
                             <div className="birth">
-                                <h5>{birth[0]}.{birth[1]}.{birth[2]}</h5>
+                                <h5>탄생일 {birth[0]}.{birth[1]}.{birth[2]}</h5>
                             </div>
-                            <div className="sns">{url}</div><br></br>
+                            <div className="sns"><a href={url}>{url}</a></div><br></br>
                         </div>
                     </div>
                     <div className="school">
                         <div className="flex">
                             <h4>학력</h4>
                             <hr></hr>
-                            {schools.map(school => <School school={school} />)}
+                            <div style={{display: "flex", paddingLeft: "30px", justifyContent: "space-around"}}>
+                                {schools.map(school => <School school={school} />)}
+                            </div>
                         </div>
                     </div>
                     <div className="career">
                         <div className="flex">
                             <h4>경력</h4>
                             <hr></hr>
-                            {companys.map(company => <Work company={company} />)}
+                            <div style={{display: "flex", paddingLeft: "30px", justifyContent: "space-around"}}>
+                                {companys && companys.map(company => <Work company={company} />)}
+                                {!companys && <span>없음</span>}
+                            </div>
                         </div>
                     </div>
                     <div style={{ width: "100%", height: "1px", margin: "30px 0px", background: "#eee" }}></div>
                     <div className="flex">
                         <div class="stack">
-                            <h4>기술스택</h4>
-                            {skills.map(skill => <Skill skill={skill} />)}
+                            <h4 style={{padding: "10px 0px"}}>기술스택</h4>
+                            <div style={{padding: "0px 30px"}}>
+                                {skills.map(skill => <Skill skill={skill} />)}
+                            </div>
                         </div>
                         <div class="lang">
                             <h4>사용언어</h4>
@@ -205,23 +216,52 @@ function Preview() {
     )
 }
 
-function toPdf() {
-    const input = document.getElementById('box');
-    const pdf = new jsPDF('p', 'mm');
-    var width = pdf.internal.pageSize.getWidth();
-    var height = pdf.internal.pageSize.getHeight();
-    if (pdf) {
-        domtoimage.toPng(input, {
-            bgcolor: 'white',
-            width: 1450,
-        }).then(
-            imgData => {
-                pdf.addImage(imgData, 'PNG', 0, 0, width, height - 10);
-                pdf.save('portfolio.pdf');
-            })
-    }
-}
+// function toPdf() {
+//     const input = document.getElementById('box');
+//     const pdf = new jsPDF('p', 'mm');
+//     var width = pdf.internal.pageSize.getWidth();
+//     var height = pdf.internal.pageSize.getHeight();
+//     if (pdf) {
+//         domtoimage.toPng(input, {
+//             bgcolor: 'white',
+//             width: 1450,
+//         }).then(
+//             imgData => {
+//                 pdf.addImage(imgData, 'PNG', 0, 0, width, height - 10);
+//                 pdf.save('portfolio.pdf');
+//             })
+//     }
+// }
 
+function toPdf() {
+    html2canvas(document.querySelector("#capture")).then(canvas => {
+        var imgData = canvas.toDataURL('image/jpeg');
+
+        var imgWidth = 170; // 이미지 가로 길이(mm) A4 기준
+        var pageHeight = imgWidth * 1.414; // 출력 페이지 세로 길이 계산 A4 기준
+        var imgHeight = canvas.height * imgWidth / canvas.width;
+        var heightLeft = imgHeight;
+        var margin = 20;
+
+        var doc = new jsPDF('p', 'mm', 'a4');
+        var position = 0;
+
+        // 첫 페이지 출력
+        doc.addImage(imgData, 'jpeg', margin, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+
+        // 한 페이지 이상일 경우 루프 돌면서 출력
+        while (heightLeft >= 20) {
+            position = heightLeft - imgHeight;
+            doc.addPage();
+            doc.addImage(imgData, 'jpeg', margin, position, imgWidth, imgHeight);
+            heightLeft -= pageHeight;
+        }
+
+        // 파일 저장
+        doc.save('gitppo.pdf');
+    });
+}
 
 
 function Result() {
